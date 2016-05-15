501941d868d645ed8a9315fc9f12a70cbbcd668a
Devin Miller <edwin0258@gmail.com>
2016-05-14 21:05:13 -0700

    Add batch file, Add C file

---
+++ HelloWorld.bat
@@ -0,0 +1,7 @@
+::Run script in Terminal with Batch File, cool
+
+@echo off
+echo "Hello World!"
+echo "To Run An .sh or .bat script in chronos terminal in chromebook use 'bash file.sh' or 'bash file.bat"
+
+pause
---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+  char* hello = "Hello World";
+  printf("%s", hello);
+  return 0;
+}
